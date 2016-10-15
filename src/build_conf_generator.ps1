dd6cbe60785445a9a5ba1bc5ba8bedc745f4b85a
Michał Jankowski <jankowski.m@gmail.com>
2016-10-15 23:12:16 +0200

    Automatic test conf generation

---
+++ src/build_conf_generator.ps1
@@ -0,0 +1,98 @@
+$fluentAssertionConfigurations = Get-Content "$PSScriptRoot/sourceConfiguration/Ver_FluentAssertion.txt"
+$fluentValidatorConfigurations = Get-Content "$PSScriptRoot/sourceConfiguration/Ver_FluentValidator.txt"
+$fluentValidatorExtConfigurations = Get-Content "$PSScriptRoot/sourceConfiguration/Ver_FluentValidator_ext.txt"
+$nugetFile = "$PSScriptRoot/sourceConfiguration/packages.config"
+$soultionFile = "$PSScriptRoot/sourceConfiguration/FluentValidation.Validators.UnitTestExtension.NuGet.Tests.csproj"
+$targetFolder = "$PSScriptRoot/testConfigurations"
+
+$configurations = New-Object System.Collections.ArrayList
+
+function SaveNewNuGetConfiguration($initialConf, $fluentValidatorExtVersion, $fluentAssertionVersion, $fluentValidatorVersion)
+{
+  [xml] $xml = Get-Content $initialConf
+  $target = $xml.SelectSingleNode('//packages/package[@id="FluentValidation.Validators.UnitTestExtension"]')
+  $target.SetAttribute("version", "$fluentValidatorExtVersion")
+  $target = $xml.SelectSingleNode('//packages/package[@id="FluentAssertions"]')
+  $target.SetAttribute("version", "$fluentAssertionVersion")
+  $target = $xml.SelectSingleNode('//packages/package[@id="FluentValidation"]')
+  $target.SetAttribute("version", "$fluentValidatorVersion")
+
+  $key = GenerateConfigurationName $fluentValidatorExtVersion $fluentAssertionVersion $fluentValidatorVersion
+  $xml.Save("$PSScriptRoot/testConfigurations/packages_"+$key+".config")
+  return $key
+}
+
+function SaveNewCsProjConfiguration($initialConf, $fluentValidatorExtVersion, $fluentAssertionVersion, $fluentValidatorVersion)
+{
+  AddReference $soultionFile "..\packages\FluentAssertions.$fluentAssertionVersion\lib\net45\FluentAssertions.dll" "FluentAssertions"
+  AddReference $soultionFile "..\packages\FluentAssertions.$fluentAssertionVersion\lib\net45\FluentAssertions.Core.dll" "FluentAssertions.Core"
+  AddReference $soultionFile "..\packages\FluentValidation.$fluentValidatorVersion.0\lib\Net45\FluentValidation.dll" "FluentValidation"
+  AddReference $soultionFile "..\packages\FluentValidation.Validators.UnitTestExtension.$fluentValidatorVersion\lib\FluentValidation.Validators.UnitTestExtension.dll" "FluentValidation.Validators.UnitTestExtension"
+}
+
+function GenerateConfigurationName($fluentValidatorExtVersion, $fluentAssertionVersion, $fluentValidatorVersion)
+{
+  return $fluentValidatorExtVersion+"_FA"+$fluentAssertionVersion+"_FV"+$fluentValidatorVersion
+}
+
+function AddReference([String]$path, [String]$dllRef, [String]$refName) 
+{
+  $proj = [xml](Get-Content $path)
+  [System.Console]::WriteLine("")
+  [System.Console]::WriteLine("AddReference {0} on {1}", $refName, $path)
+
+  # Create the following hierarchy
+  #  <Reference Include='{0}'>
+  #     <HintPath>{1}</HintPath>
+  #  </Reference>
+  # where (0) is $refName and {1} is $dllRef
+
+  $xmlns = "http://schemas.microsoft.com/developer/msbuild/2003"
+  $itemGroup = $proj.CreateElement("ItemGroup", $xmlns);
+  $proj.Project.AppendChild($itemGroup);
+
+  $referenceNode = $proj.CreateElement("Reference", $xmlns);
+  $referenceNode.SetAttribute("Include", $refName);
+  $itemGroup.AppendChild($referenceNode)
+
+  $hintPath = $proj.CreateElement("HintPath", $xmlns);
+  $hintPath.InnerXml = $dllRef
+  $referenceNode.AppendChild($hintPath)
+  $key = GenerateConfigurationName $fluentValidatorExtVersion $fluentAssertionVersion $fluentValidatorVersion
+  $proj.Save("$PSScriptRoot/testConfigurations/proj_"+$key+".csproj")
+}
+
+foreach($fluentValidatorExtConf in $fluentValidatorExtConfigurations)
+{
+  foreach($fluentAssertionConf in $fluentAssertionConfigurations)
+  {
+    $conf = SaveNewNuGetConfiguration $nugetFile $fluentValidatorExtConf $fluentAssertionConf ($fluentValidatorConfigurations  | Select-Object -first 1)
+    SaveNewCsProjConfiguration $nugetFile $fluentValidatorExtConf $fluentAssertionConf ($fluentValidatorConfigurations  | Select-Object -first 1)
+    $configurations.Add($conf)  
+  }
+
+    foreach($fluentValidatorConf in $fluentValidatorConfigurations)
+  {
+    $conf = SaveNewNuGetConfiguration $nugetFile $fluentValidatorExtConf ($fluentAssertionConfigurations  | Select-Object -first 1) $fluentValidatorConf
+    SaveNewCsProjConfiguration $nugetFile $fluentValidatorExtConf ($fluentAssertionConfigurations  | Select-Object -first 1) $fluentValidatorConf
+    $configurations.Add($conf)  
+  }
+}
+
+$stream = [System.IO.StreamWriter] "$PSScriptRoot/../.travis.yml"
+$stream.WriteLine("language: csharp")
+$stream.WriteLine("env:")
+foreach($conf in $configurations)
+{
+  $stream.WriteLine("  - CONF=$conf")
+}
+
+$stream.WriteLine("script:")
+$stream.WriteLine('  - "cp ./src/testConfigurations/packages_$CONF.config ./src/FluentValidation.Validators.UnitTestExtension.NuGet.Tests/packages.config"')
+$stream.WriteLine('  - "cp ./src/testConfigurations/proj_$CONF.csproj ./src/FluentValidation.Validators.UnitTestExtension.NuGet.Tests/FluentValidation.Validators.UnitTestExtension.NuGet.Tests.csproj"')
+$stream.WriteLine("  - sudo apt-get install -y gtk-sharp2")
+$stream.WriteLine("  - nuget restore ./src/FluentValidation.Validators.UnitTestExtension.NuGet.Tests.sln")
+$stream.WriteLine("  - xbuild /p:Configuration=Release ./src/FluentValidation.Validators.UnitTestExtension.NuGet.Tests.sln")
+$stream.WriteLine("  - mono ./src/packages/xunit.runner.console.*/tools/xunit.console.exe ./src/FluentValidation.Validators.UnitTestExtension.NuGet.Tests/bin/Release/FluentValidation.Validators.UnitTestExtension.NuGet.Tests.dll")
+$stream.close()
+
