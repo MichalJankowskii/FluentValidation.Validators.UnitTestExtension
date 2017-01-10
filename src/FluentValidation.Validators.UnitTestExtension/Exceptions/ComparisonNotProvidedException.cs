41ed6b2a194977a029e4bbcacae890d341226d97
Michał Jankowski <jankowski.m@gmail.com>
2017-01-10 21:46:59 +0100

    Update Fluent Assertions dll

--- src/FluentValidation.Validators.UnitTestExtension/Exceptions/ComparisonNotProvidedException.cs
+++ src/FluentValidation.Validators.UnitTestExtension/Exceptions/ComparisonNotProvidedException.cs
@@ -27,6 +27,8 @@
 namespace FluentValidation.Validators.UnitTestExtension.Exceptions
 {
     using System;
+
+    [Serializable]
     public class ComparisonNotProvidedException : ArgumentException
     {
         public ComparisonNotProvidedException() : base("Comparison must be provided for not build-in IComparisonValidator.")
