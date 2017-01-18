82d6927d3e47ecf5d022327f516eb6830139185b
Michał Jankowski <jankowski.m@gmail.com>
2017-01-18 17:02:07 +0100

    Update package version

--- src/FluentValidation.Validators.UnitTestExtension/Properties/SharedAssemblyInfo.cs
+++ src/FluentValidation.Validators.UnitTestExtension/Properties/SharedAssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Reflection;
 [assembly: AssemblyCopyright("Copyright Michal Jankowski©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("0.1.2.0")]
-[assembly: AssemblyFileVersion("0.1.2.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
