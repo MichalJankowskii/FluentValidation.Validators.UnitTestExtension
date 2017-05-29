9e304393850a5e50dcd7c92f786ae5eefd468352
Michał Jankowski <jankowski.m@gmail.com>
2017-05-29 23:51:57 +0200

    Update generator configuration

--- src/build_conf_generator.ps1
+++ src/build_conf_generator.ps1
@@ -27,7 +27,7 @@ function SaveNewCsProjConfiguration($initialConf, $fluentValidatorExtVersion, $f
     $proj = [xml](Get-Content $initialConf)
     AddReference $proj "..\packages\FluentAssertions.$fluentAssertionVersion\lib\net45\FluentAssertions.dll" "FluentAssertions"
     AddReference $proj "..\packages\FluentAssertions.$fluentAssertionVersion\lib\net45\FluentAssertions.Core.dll" "FluentAssertions.Core"
-    AddReference $proj "..\packages\FluentValidation.$fluentValidatorVersion.0\lib\Net45\FluentValidation.dll" "FluentValidation"
+    AddReference $proj "..\packages\FluentValidation.$fluentValidatorVersion\lib\Net45\FluentValidation.dll" "FluentValidation"
     AddReference $proj "..\packages\FluentValidation.Validators.UnitTestExtension.$fluentValidatorExtVersion\lib\net452\FluentValidation.Validators.UnitTestExtension.dll" "FluentValidation.Validators.UnitTestExtension"
     $key = GenerateConfigurationName $fluentValidatorExtVersion $fluentAssertionVersion $fluentValidatorVersion
     $proj.Save("$PSScriptRoot/testConfigurations/proj_"+$key+".csproj")
