b922af65786731ba4e367231106a19e402547124
Michal Jankowski <jankowski.m@gmail.com>
2022-06-16 20:43:19 +0200

    Added support FluentValidation > 11.0

--- src/FluentValidation.Validators.UnitTestExtension.Examples/Test/PersonValidatorTests_ClassicApproach.cs
+++ src/FluentValidation.Validators.UnitTestExtension.Examples/Test/PersonValidatorTests_ClassicApproach.cs
@@ -11,25 +11,53 @@
         [Fact]
         public void Given_FirstNameIsNull_When_Validating_Then_Error()
         {
-	        this.validator.ShouldHaveValidationErrorFor(person => person.FirstName, null as string);
+            var person = new Person
+            {
+                FirstName = null
+            };
+
+            var result = this.validator.TestValidate(person);
+
+            result.ShouldHaveValidationErrorFor(x => x.FirstName);
         }
 
         [Fact]
         public void Given_FirstNameIsEmpty_When_Validating_Then_Error()
         {
-	        this.validator.ShouldHaveValidationErrorFor(person => person.FirstName, string.Empty);
+            var person = new Person
+            {
+                FirstName = string.Empty
+            };
+
+            var result = this.validator.TestValidate(person);
+
+            result.ShouldHaveValidationErrorFor(x => x.FirstName);
         }
 
         [Fact]
         public void Given_FirstNameIsToLong_When_Validating_Then_Error()
         {
-	        this.validator.ShouldHaveValidationErrorFor(person => person.FirstName, "Long_Test_More_Than_20_Characters");
+            var person = new Person
+            {
+                FirstName = "Long_Test_More_Than_20_Characters"
+            };
+
+            var result = this.validator.TestValidate(person);
+
+            result.ShouldHaveValidationErrorFor(x => x.FirstName);
         }
 
         [Fact]
         public void Given_CorrectFirstName_When_Validating_Then_NoError()
         {
-	        this.validator.ShouldNotHaveValidationErrorFor(person => person.FirstName, "John");
+            var person = new Person
+            {
+                FirstName = "John"
+            };
+
+            var result = this.validator.TestValidate(person);
+
+            result.ShouldNotHaveValidationErrorFor(x => x.FirstName);
         }
 
         [Theory]
@@ -38,13 +66,27 @@
         [InlineData("Long_Test_More_Than_20_Characters")]
         public void Given_NotCorrectLastName_When_Validating_Then_Error(string notAcceptedText)
         {
-	        this.validator.ShouldHaveValidationErrorFor(person => person.LastName, notAcceptedText);
+            var person = new Person
+            {
+                LastName = notAcceptedText
+            };
+
+            var result = this.validator.TestValidate(person);
+
+            result.ShouldHaveValidationErrorFor(x => x.LastName);
         }
 
         [Fact]
         public void Given_CorrectLastName_When_Validating_Then_NoError()
         {
-	        this.validator.ShouldNotHaveValidationErrorFor(person => person.LastName, "John");
+            var person = new Person
+            {
+                LastName = "John"
+            };
+
+            var result = this.validator.TestValidate(person);
+
+            result.ShouldNotHaveValidationErrorFor(x => x.LastName);
         }
 
         [Theory]
@@ -53,13 +95,27 @@
         [InlineData(260)]
         public void Given_NotCorrectHeight_When_Validating_Then_Error(int height)
         {
-	        this.validator.ShouldHaveValidationErrorFor(person => person.HeightInCentimeters, height);
+            var person = new Person
+            {
+                HeightInCentimeters= height
+            };
+
+            var result = this.validator.TestValidate(person);
+
+            result.ShouldHaveValidationErrorFor(x => x.HeightInCentimeters);
         }
 
         [Fact]
         public void Given_CorrectHeight_When_Validating_Then_NoError()
         {
-	        this.validator.ShouldNotHaveValidationErrorFor(person => person.HeightInCentimeters, 150);
+            var person = new Person
+            {
+                HeightInCentimeters = 150
+            };
+
+            var result = this.validator.TestValidate(person);
+
+            result.ShouldNotHaveValidationErrorFor(x => x.HeightInCentimeters);
         }
 
 	    [Theory]
@@ -68,13 +124,27 @@
 	    [InlineData("@test")]
 	    public void Given_NotCorrectEmail_When_Validating_Then_Error(string email)
 	    {
-		    this.validator.ShouldHaveValidationErrorFor(person => person.Email, email);
+            var person = new Person
+            {
+                Email = email
+            };
+
+            var result = this.validator.TestValidate(person);
+
+            result.ShouldHaveValidationErrorFor(x => x.Email);
 	    }
 
 	    [Fact]
 	    public void Given_CorrectEmail_When_Validating_Then_NoError()
 	    {
-		    this.validator.ShouldNotHaveValidationErrorFor(person => person.Email, "test@test.pl");
+            var person = new Person
+            {
+                Email = "test@test.pl"
+            };
+
+            var result = this.validator.TestValidate(person);
+
+            result.ShouldNotHaveValidationErrorFor(x => x.Email);
 	    }
 	}
 }
